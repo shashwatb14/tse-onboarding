ea13839866ab65868ab4bdba8b5a1a6036e961be
shashwat <shashwat.bhandari.14@gmail.com>
2025-01-04 12:46:46 +0530

    Implement part 2.1

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,14 @@
+/**
+ * Task route requests.
+ */
+
+import express from "express";
+import * as UserController from "src/controllers/user";
+import * as UserValidator from "src/validators/user";
+
+const router = express.Router();
+
+router.post("/", UserValidator.createUser, UserController.createUser);
+router.get("/:id", UserController.getUser);
+
+export default router;
